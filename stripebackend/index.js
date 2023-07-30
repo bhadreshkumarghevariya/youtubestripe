3ffc19f16b0fb2e77ecdae15807915e2473410f5
Bhadreshkumar Ghevariya <bhadreshkumarghevariya@gmail.com>
2023-07-30 12:17:42 -0400

    Stripe payment done

---
+++ stripebackend/index.js
@@ -0,0 +1,95 @@
+const cors = require("cors");
+const express = require("express");
+const stripe = require("stripe")(
+  "sk_test_51NZNQ6JFJfq9k7DYsDQSLLEuT3Ydgwk2hlhlATX2ngXaf6yxIDtpj8XhOWChA0EPfo8T1luoLXhjJr8N3uKkJdgZ00Vszg9wD5"
+);
+// const uuid = require("uuid/v4");
+const { v4: uuid } = require("uuid");
+const YOUR_DOMAIN = "http://localhost:3000";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("It works!");
+});
+
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
+
+const chargeCustomer = async (customerId) => {
+  // Lookup the payment methods available for the customer
+  const paymentMethods = await stripe.paymentMethods.list({
+    customer: customerId,
+    type: "card",
+  });
+  try {
+    // Charge the customer and payment method immediately
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099,
+      currency: "cad",
+      customer: customerId,
+      payment_method: paymentMethods.data[0].id,
+      off_session: true,
+      confirm: true,
+    });
+  } catch (err) {
+    // Error code will be authentication_required if authentication is needed
+    console.log("Error code is: ", err.code);
+    const paymentIntentRetrieved = await stripe.paymentIntents.retrieve(
+      err.raw.payment_intent.id
+    );
+    console.log("PI retrieved: ", paymentIntentRetrieved.id);
+  }
+};
+
+app.post("/create-payment-intent", async (req, res) => {
+  const { items } = req.body;
+
+  // Alternatively, set up a webhook to listen for the payment_intent.succeeded event
+  // and attach the PaymentMethod to a new Customer
+  const customer = await stripe.customers.create();
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    customer: customer.id,
+    setup_future_usage: "off_session",
+    amount: calculateOrderAmount(items),
+    currency: "cad",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        price: "{{PRICE_ID}}",
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}?success=true`,
+    cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+// Listen
+app.listen(8282, () => console.log("Server is running on port 8282"));
--- stripefrontend/src/App.js
+++ stripefrontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
-function App() {
+const stripePromise = loadStripe(
+  "pk_test_51NZNQ6JFJfq9k7DY5gQBCxJy7C1febacW6bHZzqCqfBUNODuYDaBjoxIMFjZQY2BrkyKTOt5vrxJc7i4c3Ne6ZrV00Xes1Fj4Z"
+);
+export default function App() {
+  const [clientSecret, setClientSecret] = useState("");
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:8282/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stripe Payment Gateway</h1>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
     </div>
   );
 }
-
-export default App;
